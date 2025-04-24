4be3c41ea8a93dc41f20a7ad1de0838f9b1eca01
Yauheni Kuntsevich <cmaczok@gmail.com>
2025-04-24 22:32:06 +0300

    use custom tools

---
+++ custom_tools.R
@@ -0,0 +1,20 @@
+library(ellmer)
+
+chat <- chat_gemini()
+
+chat$chat("Какое сейчас время по Минску?")
+
+get_current_time <- function(tz = "UTC") {
+  format(Sys.time(), tz = tz, usetz = T)
+}
+
+chat$register_tool(tool(
+  get_current_time,
+  "Получить текущее время в указанном часовом поясе",
+  tz = type_string(
+    "Часовой пояс. По умолчанию `\"UTC\"`.",
+    required = F
+  )
+))
+
+chat$chat("Какое сейчас время по Минску?")
