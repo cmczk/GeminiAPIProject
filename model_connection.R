05c430c324c80089806dbd5b4b08634c59fb62fb
Yauheni Kuntsevich <cmaczok@gmail.com>
2025-04-24 22:00:55 +0300

    initial commit

---
+++ model_connection.R
@@ -0,0 +1,17 @@
+# install.packages("ellmer")
+library(ellmer)
+
+# .Rprofile must contain Sys.setenv(GOOGLE_API_KEY = "...")
+
+chat <- chat_gemini(
+  system_prompt = "Ты специалист по анализу данных и разработчик на языке R
+  В этом чате ты помогаешь генерировать код на языке R."
+)
+
+out <- chat$chat(
+  "Напиши функцию, которая по заданному городу
+  запрашивает текущую погоду из бесплатного API.",
+  echo = "text"
+)
+
+writeLines(text = out, con = "./out.md")
